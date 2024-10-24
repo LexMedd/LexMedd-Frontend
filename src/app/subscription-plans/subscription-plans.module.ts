66b29ee1aab965f88c697150fc302a9bf967a32c
Insonnio <102927172+Insonnio@users.noreply.github.com>
2024-10-23 22:33:52 -0500

    create suscription plans

---
+++ src/app/subscription-plans/subscription-plans.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SubscriptionPlansRoutingModule } from './subscription-plans-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    SubscriptionPlansRoutingModule
+  ]
+})
+export class SubscriptionPlansModule { }
