1fa3a95fb3957d06cf6bd902e1472a22bdb0f260
Fabiola Becerra <91694334+fabidayanee@users.noreply.github.com>
2024-10-29 01:29:50 -0500

    add components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,1 @@
-import { Component } from '@angular/core';
-import { HomeComponent } from './home.component';
-
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  standalone: true,
-  imports: [HomeComponent],
-})
-export class AppComponent {
-  title = 'my-cases';
-}
+<app-header></app-header>
---
+++ src/app/doctor-cards/doctor-cards.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-doctor-cards',  // Este selector es importante
+  templateUrl: './doctor-cards.component.html',
+  styleUrls: ['./doctor-cards.component.css']
+})
+export class DoctorCardsComponent { }
