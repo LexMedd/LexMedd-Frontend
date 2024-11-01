9aacd7f61bfeee1f4c051f596766f425813192bc
Arnold24x24 <argamoso24@hotmail.com>
2024-10-31 21:45:56 -0500

    Update footer-content.component.ts

--- src/app/public/components/footer-content/footer-content.component.ts
+++ src/app/public/components/footer-content/footer-content.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-footer-content',
   templateUrl: './footer-content.component.html',
-  styleUrls: ['./footer-content.component.css']
+  standalone: true,
+  styleUrl: './footer-content.component.css'
 })
 export class FooterContentComponent {
-  currentYear: number = new Date().getFullYear();
+
 }
